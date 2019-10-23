f5bf0d8e5df76fe87a94c148f258c7bfc2277a06
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:08:04 +0800

    第二題

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	int sum = 0 ;
-	for  (int i=1 ;i<=100;i++)
-	    sum= sum+i;
-	System.out.println(sum);
+        Scanner scn =new Scanner(System.in);
+        int n = scn.nextInt();
+        int sum = 0 ;
+        for  (int i=3 ;i<=n;i+=3)
+            sum= sum+i;
+        System.out.println(sum);
+
     }
 }
