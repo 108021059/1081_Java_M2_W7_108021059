23692a15b0cc2ac0bfe2bc03fda8b40dfb998d58
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-30 15:12:38 +0800

    M2Q4

--- src/M2Q4.java
+++ src/M2Q4.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class M2Q4 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 10; j > i; j--) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");}
-                System.out.println();
-
-        }
+            System.out.println();
+            }
 
 
     }
