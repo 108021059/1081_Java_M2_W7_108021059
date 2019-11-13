c063373363c4026f9e070febe535290f44c35eb4
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-13 14:18:31 +0800

    M2Q19

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int sum = 0;
-        for (int i = 2; i <= n; i++)
-        {
-            int j = i;
-            while (j % 2 == 0
-            ) {
-                j /= 2;
-            }
-            while (j % 3 == 0) {
-                j /= 3;
-            }
-            while (j % 5 == 0) {
-                j /= 5;
-            }
-
-            if (j == 1) {
-                sum += i;
-            }
+        int i = 1, j = 1;
+        while (i < n) {
+            System.out.print(i + "\t");
+            i = i + j;
+            j++;
         }
-        System.out.println(sum);
+        System.out.println(i);
+        i=1;
+        while (i <= n+1) {
+            for (int k=1;k<=i; k++){
+                System.out.print(i+ "\t");
+            }
+            i++;
+        }
+        System.out.println();
     }
 }
-
-
-
-
-
